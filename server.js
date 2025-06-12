2ca9de145cc37f86d76d8215284b1a26725f5231
Blazhe Manev <blaze_manev@yahoo.com>
2025-06-12 22:11:18 +0200

    update cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://smart-charge-frontend.vercel.app',
+  origin: ['https://smart-charge-frontend.vercel.app', 'http://localhost:5173' ],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
 }));
